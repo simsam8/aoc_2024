fe6206e07b5c5b31bd9409fb6224504767f965e6
simsam8 <simvedaa@gmail.com>
2024-12-04 22:09:19 +0100

    add function to create 2d tables

--- utils.lua
+++ utils.lua
@@ -6,6 +6,18 @@ function GetInput(filename)
 	return lines
 end
 
+function TableOfTables(lines)
+	local tables = {}
+	for _, line in pairs(lines) do
+		local t = {}
+		for c in string.gmatch(line, ".") do
+			table.insert(t, c)
+		end
+		table.insert(tables, t)
+	end
+	return tables
+end
+
 function SplitNumbs(line)
 	local numbs = {}
 	for i in string.gmatch(line, "%S+") do
